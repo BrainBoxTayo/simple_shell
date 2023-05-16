60ec0784f9d4171e42d2c0db65b4ede303225cf3
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-05-16 19:17:51 +0100

    added the shell

---
+++ ssshl.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <unistd.h>
+/*
+ *execve helps to execute programs
+ * first arg is filename, second is argument, third is envp
+ * hmm
+ */
+char **environ;
+int main(void)
+{
+	int flag = 1;
+	while (flag == 1)
+	{
+		size_t argc = 0;
+		int i = 0;
+		char *line = NULL, *token = NULL;
+		char *delim = " \n";
+		pid_t pid1;
+		size_t j = 10;
+		char **argv;
+
+		printf("Temitayo$ ");
+		if (getline(&line, &j, stdin) == -1)
+			printf("Error\n");
+		if (line == NULL)
+			free(line);
+
+		else
+		{
+			token = strtok(line, delim);
+			while (token)
+			{
+				token = strtok(NULL, delim);
+				argc++;
+			}
+			argv = malloc(sizeof(char *) * argc);
+			token = strtok(line, delim);
+			while (token)
+			{
+				argv[i] = token;
+				token = strtok(NULL, delim);
+				i++;
+			}
+			pid1 = fork();
+			if (pid1 == 0)
+			{
+				if (execve(argv[0], argv, environ) == -1)
+					perror("./shell");
+			}
+			else if (pid1 > 0)
+			{
+				wait(NULL);
+				
+				continue;
+			}
+			
+		}
+		free(argv), free(line);
+		}
+	return (0);
+}
